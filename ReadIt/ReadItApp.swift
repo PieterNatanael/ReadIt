d19bb0af43b01118a0f16f786893b578673fa7a8
PieterNatanael <pieternatanael@icloud.com>
2023-11-07 13:58:36 +0800

    Initial Commit

---
+++ ReadIt/ReadItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadItApp.swift
+//  ReadIt
+//
+//  Created by Pieter Yoshua Natanael on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
