415068d84f5493e70a99a852a86b83754cb21815
PieterNatanael <pieternatanael@icloud.com>
2023-11-09 15:18:58 +0800

    Initial Commit

--- ReadIt/ContentView.swift
+++ ReadIt/ContentView.swift
@@ -6,19 +6,48 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State private var textToRead = ""
+    @State private var isReading = false
+    let speechSynthesizer = AVSpeechSynthesizer()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("SayIt!")
+                .font(.largeTitle)
+                .padding()
+
+            TextEditor(text: $textToRead)
+                .padding()
+
+            Button(action: {
+                if isReading {
+                    speechSynthesizer.stopSpeaking(at: .immediate)
+                } else {
+                    speak(text: textToRead)
+                }
+                isReading.toggle()
+            }) {
+                Text(isReading ? "Stop" : "Start")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(isReading ? Color.red : Color.blue)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
+
+    func speak(text: String) {
+        let speechUtterance = AVSpeechUtterance(string: text)
+        speechSynthesizer.speak(speechUtterance)
+    }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
